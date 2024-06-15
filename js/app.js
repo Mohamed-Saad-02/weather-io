3a9301b9cfbc7006e1f45e84580c0bc9f7f3667e
Mohamed-Saad-02 <mohamedsaad63c@gmail.com>
2024-06-15 11:57:04 +0300

    initial commit

--- js/app.js
+++ js/app.js
@@ -1,242 +1,43 @@
-"use strict";
-import { fetchData, url } from "./api.js";
-import {
-  addClassName,
-  removeClassName,
-  monthNames,
-  weekDayNames,
-  getDate,
-  getHours,
-} from "./helper.js";
-
-const spinner = document.getElementById("spinner");
-
-// Active Input Search ----> Header Section <----
-const iconSearch = document.getElementById("iconSearch"),
-  iconClose = document.getElementById("iconClose"),
-  searchContainer = document.querySelector(".search.box");
-
-iconSearch.addEventListener("click", () =>
-  addClassName(searchContainer, "active")
-);
-iconClose.addEventListener("click", () =>
-  removeClassName(searchContainer, "active")
-);
-
-// fetch Data From API by Input
-
-const searchParent = document.getElementById("searchParent");
-const inputSearch = document.getElementById("searchLocation");
-
-inputSearch.addEventListener("focus", (e) => {
-  if (e.target.value.length > 2) addClassName(searchParent, "show-list");
-});
-
-inputSearch.addEventListener("blur", () => {
-  if (inputSearch.value.length <= 2) removeClassName(searchParent, "show-list");
-});
-
-let controller = new AbortController();
-
-inputSearch.addEventListener("input", (e) => {
-  const value = e.target.value;
-  if (value.length <= 2) {
-    removeClassName(searchParent, "show-list");
-    return;
-  } else addClassName(searchParent, "show-list");
-
-  // Canceled old request
-  controller.abort();
-  controller = new AbortController();
-
-  async function getData() {
-    try {
-      const res = await fetch(url.searchGeo(value), {
-        signal: controller.signal,
-      });
-
-      if (!res.ok) throw new Error("Something went wrong with fetching movies");
-
-      const data = await res.json();
-
-      handleDisplayData(data);
-    } catch (error) {
-      console.log(error.message);
-    }
-  }
-  getData();
-});
-
-// Display Data in Tag ul
-
-const listUl = document.getElementById("list");
-
-/**
- * @param {Array} data come by API from function getData in inputSearch => event input
- */
-function handleDisplayData(data) {
-  listUl.innerHTML = "";
-
-  for (const [index, city] of data.entries()) {
-    if (index > 5) return;
-    const { lat, lon, name, country } = city;
-
-    const listItem = document.createElement("li");
-    listItem.className = "list-item flex items-center justify-between gap-12";
-    listItem.dataset.location = `${lat},${lon}`;
-
-    listItem.innerHTML = `
+"use strict";import{fetchData as e,url as t}from"./api.js";import{addClassName as i,removeClassName as s,monthNames as l,weekDayNames as a,getDate as c,getHours as n}from"./helper.js";let spinner=document.getElementById("spinner"),iconSearch=document.getElementById("iconSearch"),iconClose=document.getElementById("iconClose"),searchContainer=document.querySelector(".search.box");iconSearch.addEventListener("click",()=>i(searchContainer,"active")),iconClose.addEventListener("click",()=>s(searchContainer,"active"));let searchParent=document.getElementById("searchParent"),inputSearch=document.getElementById("searchLocation");inputSearch.addEventListener("focus",e=>{e.target.value.length>2&&i(searchParent,"show-list")}),inputSearch.addEventListener("blur",()=>{inputSearch.value.length<=2&&s(searchParent,"show-list")});let controller=new AbortController;inputSearch.addEventListener("input",e=>{let l=e.target.value;if(l.length<=2){s(searchParent,"show-list");return}async function a(){try{let e=await fetch(t.searchGeo(l),{signal:controller.signal});if(!e.ok)throw Error("Something went wrong with fetching movies");let i=await e.json();handleDisplayData(i)}catch(s){console.log(s.message)}}i(searchParent,"show-list"),controller.abort(),controller=new AbortController,a()});let listUl=document.getElementById("list");function handleDisplayData(e){for(let[t,i]of(listUl.innerHTML="",e.entries())){if(t>5)return;let{lat:l,lon:a,name:c,country:n}=i,r=document.createElement("li");r.className="list-item flex items-center justify-between gap-12",r.dataset.location=`${l},${a}`,r.innerHTML=`
     <div class="icon flex items-center gap-12">
       <i class="fa-solid fa-location-dot"></i>
       <div class="item-name">
-      <p class="color-white">${name}</p>
-      <span>${country}</span>
+      <p class="color-white">${c}</p>
+      <span>${n}</span>
       </div>
     </div>
     <div class="image flex-1 flex items-center justify-end">
-      <img src="https://www.countryflags.com/wp-content/uploads/${country
-        .split(" ")
-        .join("-")
-        .toLowerCase()}-flag-png-large.png" loading="lazy" width="30" />
+      <img src="https://www.countryflags.com/wp-content/uploads/${n.split(" ").join("-").toLowerCase()}-flag-png-large.png" loading="lazy" width="30" />
     </div>
-    `;
-    listUl.appendChild(listItem);
-
-    listItem.addEventListener("click", (e) => {
-      const current = e.currentTarget;
-      const location = current.dataset.location;
-
-      updateWeather(...location.split(","));
-      inputSearch.value = "";
-      removeClassName(searchParent, "show-list");
-    });
-  }
-}
-
-// Get Current Location Of User
-const locationUser = {};
-const currentLocationBtn = document.getElementById("currentLocation");
-currentLocationBtn.addEventListener("click", currentLocation);
-function currentLocation() {
-  navigator.geolocation.getCurrentPosition(
-    (res) => {
-      const { latitude, longitude } = res.coords;
-
-      locationUser.latitude = latitude;
-      locationUser.longitude = longitude;
-
-      updateWeather(latitude, longitude);
-    },
-    (err) => {
-      console.log(err.message);
-      // Default Location
-      updateWeather(51.52, -0.11);
-    }
-  );
-}
-currentLocation();
-
-// Get Data From API and Display
-const currentWebsiteSection = document.getElementById("currentWebsite");
-const highlights = document.getElementById("highlights");
-const hourlyForecast = document.getElementById("hourlyForecast");
-const forecastSection = document.getElementById("forecastSection");
-
-/**
- * Render all weather data in html
- * @param {Number} lat Latitude
- * @param {Number} lon Longitude
- */
-
-function updateWeather(lat, lon) {
-  if (locationUser.latitude != lat && locationUser.longitude != lon)
-    document
-      .querySelector(".current-location button")
-      .removeAttribute("disabled");
-  else
-    document
-      .querySelector(".current-location button")
-      .setAttribute("disabled", "");
-
-  /**
-   * current weather section
-   */
-  fetchData(url.forecast(lat, lon, "days=5"), function (currentWeather) {
-    addClassName(document.querySelector("main"), "hidden");
-    addClassName(spinner, "active");
-
-    currentWebsiteSection.innerHTML = "";
-    highlights.innerHTML = "";
-    hourlyForecast.innerHTML = "";
-    forecastSection.innerHTML = "";
-
-    const {
-      current: {
-        condition: { icon, text },
-        temp_c: temp,
-        pressure_mb: pressure,
-        humidity,
-        feelslike_c: feels_like,
-        vis_km: visibility,
-      },
-      location: { name, localtime, country },
-      forecast: { forecastday } = {},
-    } = currentWeather;
-
-    const {
-      astro: { sunrise, sunset } = {},
-      hour = [],
-      day: {
-        air_quality: { no2, o3, so2, pm2_5 },
-      },
-    } = forecastday[0];
-
-    const card = document.createElement("div");
-    card.classList.add("card");
-
-    card.innerHTML = `
+    `,listUl.appendChild(r),r.addEventListener("click",e=>{let t=e.currentTarget,i=t.dataset.location;updateWeather(...i.split(",")),inputSearch.value="",s(searchParent,"show-list")})}}let locationUser={},currentLocationBtn=document.getElementById("currentLocation");function currentLocation(){navigator.geolocation.getCurrentPosition(e=>{let{latitude:t,longitude:i}=e.coords;locationUser.latitude=t,locationUser.longitude=i,updateWeather(t,i)},e=>{console.log(e.message),updateWeather(51.52,-.11)})}currentLocationBtn.addEventListener("click",currentLocation),currentLocation();let currentWebsiteSection=document.getElementById("currentWebsite"),highlights=document.getElementById("highlights"),hourlyForecast=document.getElementById("hourlyForecast"),forecastSection=document.getElementById("forecastSection");function updateWeather(r,d){locationUser.latitude!=r&&locationUser.longitude!=d?document.querySelector(".current-location button").removeAttribute("disabled"):document.querySelector(".current-location button").setAttribute("disabled",""),e(t.forecast(r,d,"days=5"),function(e){i(document.querySelector("main"),"hidden"),i(spinner,"active"),currentWebsiteSection.innerHTML="",highlights.innerHTML="",hourlyForecast.innerHTML="",forecastSection.innerHTML="";let{current:{condition:{icon:t,text:r},temp_c:d,pressure_mb:o,humidity:h,feelslike_c:p,vis_km:u},location:{name:g,localtime:m,country:f},forecast:{forecastday:v}={}}=e,{astro:{sunrise:x,sunset:y}={},hour:w=[],day:{air_quality:{no2:_,o3:b,so2:L,pm2_5:S}}}=v[0],$=document.createElement("div");$.classList.add("card"),$.innerHTML=`
      <h2 class="title-2">Now</h2>
      <div class="wrapper flex items-center">
-       <p class="heading">${parseInt(temp)}&deg;<sup>c</sup></p>
+       <p class="heading">${parseInt(d)}&deg;<sup>c</sup></p>
        <img
-         src="${icon}"
-         alt="${text}"
+         src="${t}"
+         alt="${r}"
          width="100"
          height="100"
          class="mx-auto"
        />
      </div>
-     <p class="body-3">${text}</p>
+     <p class="body-3">${r}</p>
      <ul class="meta-list">
        <li class="flex items-center gap-12">
          <i class="fa-regular fa-calendar color-white"></i>
-         <span>${getDate(localtime)}</span>
+         <span>${c(m)}</span>
        </li>
        <li class="flex items-center gap-12">
          <div class="flex items-center gap-12">
           <i class="fa-solid fa-location-dot color-white"></i>
-          <span data-location>${name}, ${country}</span>
+          <span data-location>${g}, ${f}</span>
          </div>
          <div class="flex-1 flex items-center justify-end">
-         <img src="https://www.countryflags.com/wp-content/uploads/${country
-           .split(" ")
-           .join("-")
-           .toLowerCase()}-flag-png-large.png" loading="lazy" width="30" />
+         <img src="https://www.countryflags.com/wp-content/uploads/${f.split(" ").join("-").toLowerCase()}-flag-png-large.png" loading="lazy" width="30" />
          </div>
        </li>
      </ul>   
-    `;
-
-    currentWebsiteSection.appendChild(card);
-
-    /**
-     * today's Highlights Section
-     */
-
-    const cardHighlights = document.createElement("div");
-    cardHighlights.className = "card";
-
-    cardHighlights.innerHTML = `
+    `,currentWebsiteSection.appendChild($);let E=document.createElement("div");E.className="card",E.innerHTML=`
               <div class="title-2">Todays Highlights</div>
               <div class="highlight-list gap-20">
                 <div
@@ -249,22 +50,22 @@ function updateWeather(lat, lon) {
                     </div>
                     <ul class="card-list flex items-center flex-1 flex-wrap">
                       <li class="card-item flex items-center gap-8">
-                        <p class="title-1">${Number(pm2_5).toFixed(1)}</p>
+                        <p class="title-1">${Number(S).toFixed(1)}</p>
                         <p class="label-1">PM<sub>2.5</sub></p>
                       </li>
 
                       <li class="card-item flex items-center gap-8">
-                        <p class="title-1">${Number(so2).toFixed(1)}</p>
+                        <p class="title-1">${Number(L).toFixed(1)}</p>
                         <p class="label-1">SO<sub>2</sub></p>
                       </li>
 
                       <li class="card-item flex items-center gap-8">
-                        <p class="title-1">${Number(no2).toFixed(1)}</p>
+                        <p class="title-1">${Number(_).toFixed(1)}</p>
                         <p class="label-1">NO<sub>2</sub></p>
                       </li>
 
                       <li class="card-item flex items-center gap-8">
-                        <p class="title-1">${Number(o3).toFixed(1)}</p>
+                        <p class="title-1">${Number(b).toFixed(1)}</p>
                         <p class="label-1">O<sub>3</sub></p>
                       </li>
                     </ul>
@@ -279,14 +80,14 @@ function updateWeather(lat, lon) {
                       <i class="fa-regular fa-sun"></i>
                       <div class="info">
                         <div class="label-1">Sunrise</div>
-                        <div class="title-1">${sunrise}</div>
+                        <div class="title-1">${x}</div>
                       </div>
                     </div>
                     <div class="sunset flex items-center flex-1 gap-20">
                       <i class="fa-regular fa-moon"></i>
                       <div class="info">
                         <div class="label-1">Sunset</div>
-                        <div class="title-1">${sunset}</div>
+                        <div class="title-1">${y}</div>
                       </div>
                     </div>
                   </div>
@@ -304,7 +105,7 @@ function updateWeather(lat, lon) {
                         height="40"
                       />
                     </div>
-                    <p class="title-1">${humidity}<sub>%</sub></p>
+                    <p class="title-1">${h}<sub>%</sub></p>
                   </div>
                 </div>
                 <div
@@ -320,7 +121,7 @@ function updateWeather(lat, lon) {
                         height="40"
                       />
                     </div>
-                    <p class="title-1">${pressure}<sub>hpa</sub></p>
+                    <p class="title-1">${o}<sub>hpa</sub></p>
                   </div>
                 </div>
                 <div
@@ -331,7 +132,7 @@ function updateWeather(lat, lon) {
                     <div class="icon">
                       <i class="fa-solid fa-eye"></i>
                     </div>
-                    <p class="title-1">${visibility}<sub>km</sub></p>
+                    <p class="title-1">${u}<sub>km</sub></p>
                   </div>
                 </div>
                 <div
@@ -347,64 +148,33 @@ function updateWeather(lat, lon) {
                         height="40"
                       />
                     </div>
-                    <p class="title-1">${parseInt(
-                      feels_like
-                    )}&deg;<sup>c</sup></p>
+                    <p class="title-1">${parseInt(p)}&deg;<sup>c</sup></p>
                   </div>
                 </div>
               </div>
-      `;
-
-    highlights.appendChild(cardHighlights);
-
-    /**
-     * forecast Section
-     */
-
-    hourlyForecast.innerHTML = `
+      `,highlights.appendChild(E),hourlyForecast.innerHTML=`
           <h2 class="title-2">Today at</h2>
           <div class="slider-container">
             <ul class="slider-list flex items-center gap-20" data-temp></ul>
             <ul class="slider-list flex items-center gap-20" data-wind></ul>
           </div>
-      `;
-
-    for (let i = 3; i < hour.length; i += 3) {
-      const {
-        condition: { icon, text },
-        temp_c: temp,
-        time_epoch: dateUnix,
-        wind_degree: windDirection,
-        wind_kph: windSpeed,
-      } = hour[i];
-
-      const tempLi = document.createElement("li");
-      tempLi.className = "slider-item flex-1";
-
-      tempLi.innerHTML = `
+      `;for(let T=3;T<w.length;T+=3){let{condition:{icon:j,text:C},temp_c:H,time_epoch:M,wind_degree:B,wind_kph:F}=w[T],I=document.createElement("li");I.className="slider-item flex-1",I.innerHTML=`
           <div class="card slider-card flex flex-column items-center">
-            <p class="body-3">${getHours(dateUnix)}</p>
+            <p class="body-3">${n(M)}</p>
             <img
-              src="${icon}"
-              title="${text}"
-              alt="${text}"
+              src="${j}"
+              title="${C}"
+              alt="${C}"
               width="80"
               height="80"
               loading="lazy"
               class="weather-icon"
             />
-            <p class="body-3">${parseInt(temp)}&deg;</p>
+            <p class="body-3">${parseInt(H)}&deg;</p>
           </div>
-        `;
-
-      hourlyForecast.querySelector("[data-temp]").appendChild(tempLi);
-
-      const windLi = document.createElement("li");
-      windLi.className = "slider-item flex-1";
-
-      windLi.innerHTML = `
+        `,hourlyForecast.querySelector("[data-temp]").appendChild(I);let k=document.createElement("li");k.className="slider-item flex-1",k.innerHTML=`
           <div class="card slider-card flex flex-column items-center">
-            <p class="body-3">${getHours(dateUnix)}</p>
+            <p class="body-3">${n(M)}</p>
             <div class="icon flex items-center justify-center">
               <img
               src="./images/direction.png"
@@ -414,60 +184,28 @@ function updateWeather(lat, lon) {
               height="48"
               loading="lazy"
               class="weather-icon direction"
-              style="transform: rotate(${windDirection - 180}deg);"
+              style="transform: rotate(${B-180}deg);"
               />
             </div>
-            <p class="body-3">${parseInt(windSpeed)}/h</p>
+            <p class="body-3">${parseInt(F)}/h</p>
           </div>
-        `;
-      hourlyForecast.querySelector("[data-wind]").appendChild(windLi);
-    }
-
-    forecastSection.innerHTML = `
+        `,hourlyForecast.querySelector("[data-wind]").appendChild(k)}for(let P of(forecastSection.innerHTML=`
         <h2 class="title-2">5 Days Forecast</h2>
         <div class="card">
           <ul data-forecast-list></ul>
         </div>
-      `;
-
-    for (const data of forecastday) {
-      const {
-        date_epoch: dt_unix,
-        day: {
-          condition: { icon, text },
-          maxtemp_c: temp_max,
-        },
-      } = data;
-
-      const date = new Date(dt_unix * 1000);
-
-      const li = document.createElement("li");
-      li.className = "card-item flex items-center";
-
-      li.innerHTML = `
+      `,v)){let{date_epoch:q,day:{condition:{icon:D,text:U},maxtemp_c:W}}=P,N=new Date(1e3*q),z=document.createElement("li");z.className="card-item flex items-center",z.innerHTML=`
           <div class="icon flex items-center">
             <img
-              src="${icon}"
-              alt="${text}"
+              src="${D}"
+              alt="${U}"
               width="60"
               height="60"
-              title="${text}"
+              title="${U}"
               loading="lazy"
             />
-            <span class="degree">${parseInt(temp_max)}&deg;</span>
+            <span class="degree">${parseInt(W)}&deg;</span>
           </div>
-          <p class="flex-1 text-right">${date.getDate()} ${
-        monthNames[date.getMonth()]
-      }</p>
-          <p class="flex-1 text-right">${weekDayNames[date.getDay()]}</p>
-        `;
-
-      forecastSection.querySelector("[data-forecast-list]").appendChild(li);
-    }
-
-    removeClassName(document.querySelector("main"), "hidden");
-    setTimeout(() => {
-      removeClassName(spinner, "active");
-    }, 500);
-  });
-}
+          <p class="flex-1 text-right">${N.getDate()} ${l[N.getMonth()]}</p>
+          <p class="flex-1 text-right">${a[N.getDay()]}</p>
+        `,forecastSection.querySelector("[data-forecast-list]").appendChild(z)}s(document.querySelector("main"),"hidden"),setTimeout(()=>{s(spinner,"active")},500)})}
